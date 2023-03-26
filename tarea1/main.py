709cc3fb9aa1f3bb93450c4fd81c955987b10c19
Joacker <jfernandezvizcarra@gmail.com>
2023-03-26 16:05:19 -0300

    new commit

---
+++ tarea1/main.py
@@ -0,0 +1,20 @@
+# make a nonogram solver
+
+'''
+                        3  7  7  3
+               4  2  7  4  2  2  4  7  2  4
+            4           X  X  X  X
+            8     X  X  X  X  X  X  X  X
+           10  X  X  X  X  X  X  X  X  X  X
+1  1  2  1  1  X     X     X  X     X     X
+1  1  2  1  1  X     X     X  X     X     X
+      1  6  1  X     X  X  X  X  X  X     X
+            6        X  X  X  X  X  X
+         2  2        X  X        X  X  
+            4           X  X  X  X
+            2              X  X
+'''
+
+
+if __name__ == '__main__':
+    print("Hello World")
