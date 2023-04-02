35c53c07db776f9bb5b3fc22e3e08162ec641b2e
Joacker <jfernandezvizcarra@gmail.com>
2023-04-01 23:23:53 -0400

    new heuristic

--- tarea1/python/version2/main.py
+++ tarea1/python/version2/main.py
@@ -1,3 +1,20 @@
+# Tarea 1 Ze Hui Fu y Joaquín Fernández
+
+'''
+                        3  7  7  3
+               4  2  7  4  2  2  4  7  2  4
+            4           X  X  X  X
+            8     X  X  X  X  X  X  X  X
+           10  X  X  X  X  X  X  X  X  X  X
+1  1  2  1  1  X     X     X  X     X     X
+1  1  2  1  1  X     X     X  X     X     X
+      1  6  1  X     X  X  X  X  X  X     X
+            6        X  X  X  X  X  X
+         2  2        X  X        X  X  
+            4           X  X  X  X
+            2              X  X
+'''
+
 import itertools
 import time
 
